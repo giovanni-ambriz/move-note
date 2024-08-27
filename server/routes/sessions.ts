32f9464abc819085f666b6203eac596ce4f3f980
Giovanni Ambriz <giovanni.ambriz@hotmail.com>
2024-08-27 21:22:59 +1200

    Add route functions

---
+++ server/routes/sessions.ts
@@ -0,0 +1,90 @@
+import { Router } from 'express'
+// // import checkJwt, { JwtRequest } from '../auth0.ts'
+// import { StatusCodes } from 'http-status-codes'
+
+import * as db from '../db/db.ts'
+
+const router = Router()
+
+// GET '/api/v1/sessions'
+
+router.get('/', async (req, res) => {
+  try {
+    const sessions = await db.getAllSessions()
+    res.json(sessions)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+// GET '/api/v1/sessions/:id'
+
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const sessions = await db.getSessionById(id)
+    res.json(sessions)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+// GET '/api/v1/sessions/activity/:id'
+
+router.get('/activity/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const sessions = await db.getSessionsByActivity(id)
+    res.json(sessions)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+// GET '/api/v1/sessions/:id'
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const { notes } = req.body
+    const noteAdded = await db.addNote(Number(id), notes)
+    res.json(noteAdded)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+// GET '/api/v1/sessions/:id'
+
+router.delete('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    await db.deleteSession(id)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+// router.post('/', checkJwt, async (req: JwtRequest, res, next) => {
+//   if (!req.auth?.sub) {
+//     res.sendStatus(StatusCodes.UNAUTHORIZED)
+//     return
+//   }
+
+//   try {
+//     const { owner, name } = req.body
+//     const id = await db.addFruit({ owner, name })
+//     res
+//       .setHeader('Location', `${req.baseUrl}/${id}`)
+//       .sendStatus(StatusCodes.CREATED)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-import fruitRoutes from './routes/fruits.ts'
+import sessionRoutes from './routes/sessions.ts'
 
 const server = express()
 
 server.use(express.json())
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/sessions', sessionRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
