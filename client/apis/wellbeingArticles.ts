78abb11595a0945504b103cc386b7d396e1942cd
Giovanni Ambriz <giovanni.ambriz@hotmail.com>
2024-12-12 15:39:02 +1300

    Update endpoint for wellbeing API to match new version API requirements

--- client/apis/wellbeingArticles.ts
+++ client/apis/wellbeingArticles.ts
@@ -1,13 +1,12 @@
 import request from 'superagent'
 
-const API_BASE_URL = 'https://api.nhs.uk/live-well'
-const API_VERSION = '1.0'
+const API_BASE_URL =
+  'https://int.api.service.nhs.uk/nhs-website-content/live-well'
 
 export async function fetchWellbeingArticles(): Promise<any> {
   const res = await request
     .get(`${API_BASE_URL}/`)
-    .query({ 'api-version': API_VERSION })
-    .set('subscription-key', 'b1541916e2324b8caf77e05d3ef6ced7')
+    .set('apikey', 'MkM6TY341thYlwlTbFGRparA6FrldrK4')
 
   return res.body
 }
